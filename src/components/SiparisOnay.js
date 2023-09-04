5cea20725c8b2a09b13dba489a508bba48efba77
Münir BESYE <imunirbesye@gmail.com>
2023-09-04 21:18:43 +0300

    yapildi

---
+++ src/components/SiparisOnay.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './SiparisOnay.css';
+
+const SiparisOnay = () => {
+    return (
+        <header>
+            <h1>Teknolojik Yemekler</h1>
+            <span className='tebrikler-mesaj'>TEBRIKLER!<br />SİPARİŞİNİZ ALINDI!</span>
+        </header>
+    );
+};
+
+export default SiparisOnay;
